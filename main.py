952bec287ffaefa6db594ee9d7a4e47e22a2d913
NadyaEvp <t_evp@mail.ru>
2024-10-17 18:54:01 +0300

    download books with titles

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 import os
+from pathvalidate import sanitize_filename
 
 
-def save_file(book_id, directory):
-    url = f"https://tululu.org/txt.php?id={book_id}"
-    response = requests.get(url)
-    response.raise_for_status()
+def get_response(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
 
-    filename = f"id_{book_id+1}.txt"
-    with open(f"{directory}/{filename}", "wb") as file:
-        file.write(response.content)
+        if response.content:
+            return response
+
+    except Exception as error:
+        print(error)
 
 
 def create_path(directory):
@@ -17,11 +21,51 @@ def create_path(directory):
         os.makedirs(directory)
 
 
+def get_info_book(url_book):
+    try:
+        response = get_response(url_book)
+        soup = BeautifulSoup(response.text, "lxml")
+        content_text, content_auhtor = soup.select_one(
+            "body div[id=content] h1"
+        ).text.split(" :: ")
+        href = soup.select_one(
+            ".d_book a[title*='скачать книгу txt']"
+        )["href"]
+
+        if not href:
+            raise Exception
+
+        content_text = content_text.strip("\xa0").strip(" ")
+
+        return content_text
+
+    except Exception as error:
+        print(error)
+
+
+def download_txt(url, filename, book_id, folder="books/"):
+    filename = f"{book_id+1}.{filename}.txt"
+    response = get_response(url)
+    filename = sanitize_filename(filename)
+    filepath = os.path.join(folder, filename)
+    with open(filepath, "wb") as file:
+        file.write(response.content)
+
+    return filepath
+
+
 def main():
     directory = "books"
     create_path(directory)
     for book_id in range(10):
-        save_file(book_id, directory)
+        url = f"https://tululu.org/txt.php?id={book_id+1}"
+        url_book = f"https://tululu.org/b{book_id+1}"
+        filename = get_info_book(url_book)
+        if filename is None:
+            print('This book is not on the site yet')
+        else:
+            filepath = download_txt(url, filename, book_id, folder="books/")
+            print(filepath)
 
 
 if "__main__" == __name__:
