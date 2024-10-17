094bbdeb6ddab16d138b977cb06ff784bdec5f2b
NadyaEvp <t_evp@mail.ru>
2024-10-17 19:38:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin
 
 
 def get_response(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+    response = requests.get(url)
+    response.raise_for_status()
 
-        if response.content:
-            return response
-
-    except Exception as error:
-        print(error)
+    if response.content:
+        return response
 
 
 def create_path(directory):
@@ -25,9 +22,6 @@ def get_info_book(url_book):
     try:
         response = get_response(url_book)
         soup = BeautifulSoup(response.text, "lxml")
-        content_text, content_auhtor = soup.select_one(
-            "body div[id=content] h1"
-        ).text.split(" :: ")
         href = soup.select_one(
             ".d_book a[title*='скачать книгу txt']"
         )["href"]
@@ -35,12 +29,19 @@ def get_info_book(url_book):
         if not href:
             raise Exception
 
+        content_text, content_auhtor = soup.select_one(
+            "body div[id=content] h1"
+        ).text.split(" :: ")
+        img = soup.find("div", class_="bookimage").find("a").find("img")["src"]
         content_text = content_text.strip("\xa0").strip(" ")
+        url_book = urljoin(url_book, img)
+
+        print(content_text)
 
         return content_text
 
-    except Exception as error:
-        print(error)
+    except Exception:
+        pass
 
 
 def download_txt(url, filename, book_id, folder="books/"):
@@ -48,10 +49,9 @@ def download_txt(url, filename, book_id, folder="books/"):
     response = get_response(url)
     filename = sanitize_filename(filename)
     filepath = os.path.join(folder, filename)
+    print(filepath)
     with open(filepath, "wb") as file:
         file.write(response.content)
-
-    return filepath
 
 
 def main():
@@ -62,10 +62,9 @@ def main():
         url_book = f"https://tululu.org/b{book_id+1}"
         filename = get_info_book(url_book)
         if filename is None:
-            print('This book is not on the site yet')
+            continue
         else:
-            filepath = download_txt(url, filename, book_id, folder="books/")
-            print(filepath)
+            download_txt(url, filename, book_id, folder="books/")
 
 
 if "__main__" == __name__:
