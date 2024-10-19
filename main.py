be5b13d361ef8c2eda701d2fb1c72a8334d930b4
NadyaEvp <t_evp@mail.ru>
2024-10-19 16:38:00 +0300

    download book covers

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from pathvalidate import sanitize_filename
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 
 def get_response(url):
@@ -18,13 +18,11 @@ def create_path(directory):
         os.makedirs(directory)
 
 
-def get_info_book(url_book):
+def get_name_book(url_book):
     try:
         response = get_response(url_book)
         soup = BeautifulSoup(response.text, "lxml")
-        href = soup.select_one(
-            ".d_book a[title*='скачать книгу txt']"
-        )["href"]
+        href = soup.select_one(".d_book a[title*='скачать книгу txt']")["href"]
 
         if not href:
             raise Exception
@@ -32,11 +30,7 @@ def get_info_book(url_book):
         content_text, content_auhtor = soup.select_one(
             "body div[id=content] h1"
         ).text.split(" :: ")
-        img = soup.find("div", class_="bookimage").find("a").find("img")["src"]
         content_text = content_text.strip("\xa0").strip(" ")
-        url_book = urljoin(url_book, img)
-
-        print(content_text)
 
         return content_text
 
@@ -44,27 +38,53 @@ def get_info_book(url_book):
         pass
 
 
+def get_img_book(url_book):
+    try:
+        response = get_response(url_book)
+        soup = BeautifulSoup(response.text, "lxml")
+        img = soup.find("div", class_="bookimage").find("a").find("img")["src"]
+        url_book = urljoin(url_book, img)
+        return url_book
+
+    except Exception:
+        pass
+
+
 def download_txt(url, filename, book_id, folder="books/"):
+    create_path(folder)
     filename = f"{book_id+1}.{filename}.txt"
     response = get_response(url)
     filename = sanitize_filename(filename)
     filepath = os.path.join(folder, filename)
-    print(filepath)
+    with open(filepath, "wb") as file:
+        file.write(response.content)
+
+
+def download_image(url, img, folder="images/"):
+    create_path(folder)
+    filename = urlparse(img)
+    filename = os.path.basename(filename.path)
+    filename = sanitize_filename(filename)
+    filepath = os.path.join(folder, filename)
+
+    response = get_response(img)
+    response.raise_for_status()
+
     with open(filepath, "wb") as file:
         file.write(response.content)
 
 
 def main():
-    directory = "books"
-    create_path(directory)
     for book_id in range(10):
         url = f"https://tululu.org/txt.php?id={book_id+1}"
         url_book = f"https://tululu.org/b{book_id+1}"
-        filename = get_info_book(url_book)
+        filename = get_name_book(url_book)
+        img = get_img_book(url_book)
         if filename is None:
             continue
         else:
-            download_txt(url, filename, book_id, folder="books/")
+            download_txt(url, filename, book_id)
+            download_image(url, img)
 
 
 if "__main__" == __name__:
