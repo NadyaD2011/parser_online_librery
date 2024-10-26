96289a0af479bc2841afb66e9b6c5f9246bd7701
NadyaEvp <t_evp@mail.ru>
2024-10-26 18:14:16 +0300

    Разбираемся с перехватом ошибок

--- tululu.py
+++ tululu.py
@@ -11,69 +11,60 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_txt(url, filename, book_id, folder="books/"):
+def download_txt(response, filename, book_id, folder="books/"):
     os.makedirs(folder, exist_ok=True)
     filename = f"{book_id}.{filename}.txt"
-    response = get_response(url)
     filepath = os.path.join(folder, filename)
 
     with open(filepath, "wb") as file:
         file.write(response.content)
 
 
-def download_image(img, folder="images/"):
+def download_image(img, book_url, folder="images/"):
     os.makedirs(folder, exist_ok=True)
     filename = urlparse(img)
     filename = os.path.basename(filename.path)
     filename = sanitize_filename(filename)
     filepath = os.path.join(folder, filename)
 
-    response = get_response(img)
+    img_url = urljoin(book_url, img)
+    response = requests.get(img_url)
     response.raise_for_status()
+    check_for_redirect(response)
 
     with open(filepath, "wb") as file:
         file.write(response.content)
 
 
-def parse_book_page(book_page, book_url):
-    try:
-        soup = BeautifulSoup(book_page, "lxml")
-        comments = []
-        genres = []
+def parse_book_page(page_response):
+    soup = BeautifulSoup(page_response.text, "lxml")
+    comments = []
+    genres = []
+    
+    title, author = soup.select_one("body div[id=content] h1").text.split(
+        " \xa0 :: \xa0 "
+    )
+    title = sanitize_filename(title)
 
-        href = soup.select_one(".d_book a[title*='скачать книгу txt']")["href"]
+    img = soup.find("div", class_="bookimage").find("a").find("img")["src"]
 
-        if not href:
-            raise Exception
+    all_comments = soup.find_all("div", class_="texts")
+    for comment in all_comments:
+        comments.append(comment.find("span").text)
 
-        title, author = soup.select_one("body div[id=content] h1").text.split(
-            " \xa0 :: \xa0 "
-        )
-        title = sanitize_filename(title)
+    genres_site = soup.find("span", class_="d_book").find_all("a")
+    for genre in genres_site:
+        genres.append(genre.text)
 
-        img = soup.find("div", class_="bookimage").find("a").find("img")["src"]
-        cover_path = urljoin(book_url, img)
+    book_data = {
+        "author": author,
+        "title": title,
+        "cover_path": img,
+        "comments": comments,
+        "genres": genres,
+    }
 
-        all_comments = soup.find_all("div", class_="texts")
-        for comment in all_comments:
-            comments.append(comment.find("span").text)
-
-        genres_site = soup.find("span", class_="d_book").find_all("a")
-        for genre in genres_site:
-            genres.append(genre.text)
-
-        book_data = {
-            "author": author,
-            "title": title,
-            "cover_path": cover_path,
-            "comments": comments,
-            "genres": genres,
-        }
-
-        return book_data
-
-    except Exception:
-        pass
+    return book_data
 
 
 def main():
@@ -90,18 +81,26 @@ def main():
 
     for book_id in range(args.start_id, args.end_id):
         url = "https://tululu.org/txt.php"
-        url_book_site = f"https://tululu.org/b{book_id}"
-        params = {'id': book_id}
-        book_page = get_response(url_book_site, params)
-        book_page = book_page.text
-        book_data = parse_book_page(book_page, url_book_site)
-        if book_data is None:
-            continue
-        elif book_data is None:
-            continue
-        else:
-            download_txt(url, book_data["title"], book_id)
-            download_image(book_data["cover_path"])
+        url_book_site = f"https://tululu.org/b{book_id}/"
+        params = {"id": book_id}      
+        try:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            check_for_redirect(response)
+
+            page_response = requests.get(url_book_site)
+            page_response.raise_for_status()
+            check_for_redirect(page_response)
+
+            book_data = parse_book_page(page_response)
+
+            download_txt(response, book_data["title"], book_id)
+            download_image(book_data["cover_path"], url_book_site)
+
+        except requests.HTTPError:
+            print("Книга не найдена")
+        except requests.ConnectionError:
+            print("Произошла ошибка подключения.")
 
 
 if "__main__" == __name__:
