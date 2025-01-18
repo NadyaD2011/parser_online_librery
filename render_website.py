59016229bb2f68aae69d4ba9d49a33c9a105edbb
NadyaEvp <t_evp@mail.ru>
2025-01-18 18:52:18 +0300

    Убираем лишний код

--- render_website.py
+++ render_website.py
@@ -17,6 +17,7 @@ def read_json(name_json_file="book_elements.json"):
 def on_reload():
     page_folder = "pages"
     name_json_file = "book_elements.json"
+    number_books = 10
     os.makedirs(page_folder, exist_ok=True)
     env = Environment(
         loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
@@ -24,16 +25,16 @@ def on_reload():
 
     template = env.get_template("/template/template.html")
     book_elements = read_json(name_json_file)
-    books_pages = list(chunked(book_elements, 10))
+    books_pages = list(chunked(book_elements, number_books))
     total_pages = len(books_pages)
 
-    for number, books_page in enumerate(books_pages):
+    for number, books_page in enumerate(books_pages, 1):
         rendered_page = template.render(
             paired_books=chunked(books_page, 2),
-            page_number=number + 1,
+            page_number=number,
             total_pages=total_pages,
         )
-        page_path = f"{page_folder}/index{number + 1}.html"
+        page_path = f"{page_folder}/index{number}.html"
         with open(page_path, "w", encoding="utf8") as file:
             file.write(rendered_page)
 
